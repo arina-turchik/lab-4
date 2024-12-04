5116d0da8cc7f7b1339ecc0298580321f02fe0e7
Arina Turchik <arina.turchik@mail.ru>
2024-12-05 01:46:16 +0500

    commit 00.1

--- code/00_distance.py
+++ code/00_distance.py
@@ -14,7 +14,14 @@ sites = {
 
 distances = {}
 
-# TODO здесь заполнение словаря
+# Заполняем словарь расстояний
+for city1, coords1 in sites.items():
+    distances[city1] = {}
+    for city2, coords2 in sites.items():
+        if city1 != city2:  # Не вычисляем расстояние до самого себя
+            # Вычисление расстояния
+            distance = ((coords1[0] - coords2[0]) ** 2 + (coords1[1] - coords2[1]) ** 2) ** 0.5
+            distances[city1][city2] = distance # Сохраняем расстояние в словаре
 
 print(distances)
 
